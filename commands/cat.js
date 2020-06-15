2a28ff6ae8453eefbcd57f6e0b39b76786ab93cf
Antti <antti@vmi326928.contaboserver.net>
2020-06-15 18:19:27 +0200

    gif option for cat command

--- commands/cat.js
+++ commands/cat.js
@@ -1,7 +1,7 @@
 const bent = require("bent")
 const Discord = require("discord.js")
 exports.execute = async (client, message, args) => {
-    let body = await bent("GET", 200, "json", {"x-api-key": client.config.thecatapi})("https://api.thecatapi.com/v1/images/search")
+    let body = await bent("GET", 200, "json", {"x-api-key": client.config.thecatapi})("https://api.thecatapi.com/v1/images/search" + (args[0]=="gif"?"?mime_types=gif":""))
     let embed = new Discord.MessageEmbed()
     .setTitle("UwU here's a cat for u")
     .setImage(body[0].url)
@@ -15,6 +15,6 @@ exports.data = {
     category: "fun",
     name: "cat",
     desc: "Nice cat pics uwu",
-    usage: "cat",
+    usage: "cat [gif]",
     perm: 0
 };
