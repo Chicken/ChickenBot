11d5df2028adf25f76f6355f6bc9dbb532b3d00d
Antti <antti@vmi326928.contaboserver.net>
2020-06-20 21:29:35 +0200

    new url

--- commands/ytdl.js
+++ commands/ytdl.js
@@ -23,7 +23,7 @@ exports.execute = async (client, message, args) => {
     await message.channel.send("Downloading... I'll ping you when ready!")
 
     if(fs.existsSync("/home/antti/ChickenBot/ytdl/" + (audioOnly?"audio":"video") + "/" + info.player_response.videoDetails.videoId + (audioOnly?".mp3":".mp4"))) {
-        return message.reply("Done! Here's a link you can download from! (expires someday in the future) <http://antti.codes:8080/ytdl/" + (audioOnly?"audio":"video") + "/" + info.player_response.videoDetails.videoId + ">")
+        return message.reply("Done! Here's a link you can download from! (expires someday in the future) <https://chickenbot.antti.codes/ytdl/" + (audioOnly?"audio":"video") + "/" + info.player_response.videoDetails.videoId + ">")
     }
 
     if(audioOnly) {
@@ -32,11 +32,11 @@ exports.execute = async (client, message, args) => {
         .audioBitrate(128)
         .save("/home/antti/ChickenBot/ytdl/audio/" + info.player_response.videoDetails.videoId + ".mp3")
         .on("end", ()=>{
-            return message.reply("Done! Here's a link you can download from! (expires someday in the future) <http://antti.codes:8080/ytdl/audio/" + info.player_response.videoDetails.videoId + ">")
+            return message.reply("Done! Here's a link you can download from! (expires someday in the future) <https://chickenbot.antti.codes/ytdl/audio/" + info.player_response.videoDetails.videoId + ">")
         })
     } else {
         ytdl.downloadFromInfo(info, { filter: format => format.container === "mp4", quality: "highest", highWaterMark: 8388608 }).pipe(fs.createWriteStream("/home/antti/ChickenBot/ytdl/video/" + info.player_response.videoDetails.videoId + ".mp4")).on("close", ()=>{
-            return message.reply("Done! Here's a link you can download from! (expires someday in the future) <http://antti.codes:8080/ytdl/video/" + info.player_response.videoDetails.videoId + ">")
+            return message.reply("Done! Here's a link you can download from! (expires someday in the future) <https://chickenbot.antti.codes/ytdl/video/" + info.player_response.videoDetails.videoId + ">")
         })
     }
 }
