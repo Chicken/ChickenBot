99860de93d442f72f9d6d99b7c88a61f89c4b7e1
Antti <antti@vmi326928.contaboserver.net>
2020-07-06 11:01:21 +0200

    eval much more compact

--- commands/eval.js
+++ commands/eval.js
@@ -1,54 +1,28 @@
 exports.execute = async (client, message, args) => {
     if(message.author.id !== client.config.owner && !client.config.admins.includes(message.author.id)) return;
-    if(message.author.id !== client.config.owner) {
-        client.users.cache.get("312974985876471810").send(message.author.tag + " used eval.\n```js\n" + args.join(" ") + "\n```")
-    }
+    if(message.author.id !== client.config.owner) client.users.cache.get("312974985876471810").send(message.author.tag + " used eval.\n```js\n" + args.join(" ") + "\n```");
 
-    let flags = []
-
+    let flags = [], res, diff, start, us, async1, async2;
     while(args[0].startsWith("-")) {
         flags.push(args.shift()[1])
     }
-
     if(flags.includes("S")) message.delete();
 
     try{
-        let res, diff, start, us, time, unit;
-
+        async1 = (flags.includes("A")?"(async()=>{":"")
+        async2 = (flags.includes("A")?"})()":"")
         if(flags.includes("a")) {
-            if(flags.includes("A")) {
-                start = process.hrtime();
-                res = await eval("(async()=>{"+args.join(" ")+"})()");
-                diff = process.hrtime(start);
-            } else {
-                start = process.hrtime();
-                res = await eval(args.join(" "));
-                diff = process.hrtime(start);
-            }
+            start = process.hrtime();
+            res = await eval(async1+args.join(" ")+async2)
+            diff = process.hrtime(start);
         } else {
-            if(flags.includes("A")) {
-                start = process.hrtime();
-                res = eval("(async()=>{"+args.join(" ")+"})()");
-                diff = process.hrtime(start);
-            } else {
-                start = process.hrtime();
-                res = eval(args.join(" "));
-                diff = process.hrtime(start);
-            }
+            start = process.hrtime();
+            res = eval(async1+args.join(" ")+async2)
+            diff = process.hrtime(start);
         }
-
         us = diff[0] * 1000000 + diff[1] / 1000;
 
-
-        if (us>1000) {
-            unit = "ms";
-            time = (us / 1000).toFixed(3); 
-        } else {
-            unit = "μs";
-            time = us.toFixed(3);
-        }
-
-        if(!flags.includes("s")) message.channel.send(`**SUCCESS**\n\`\`\`js\n${require("util").inspect(res, {depth: 3}).substring(0, 1800)}\n\`\`\`\n**Executed in**\n\`${time}\`${unit}`)
+        if(!flags.includes("s")) message.channel.send(`**SUCCESS**\n\`\`\`js\n${require("util").inspect(res, {depth: 3}).substring(0, 1800)}\n\`\`\`\n**Executed in**\n\`${us>1000?(us/1000).toFixed(3):us.toFixed(3)}\`${us>1000?"ms":"μs"}`)
     } catch(e) {
         if(!flags.includes("s")) message.channel.send(`**ERROR**\n\`\`\`js\n${e}\n\`\`\``)
     }
