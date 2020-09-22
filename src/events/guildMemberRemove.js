21cd0ef627d04135a9e2e3b06b071c94f90bfb7c
Chicken <antti@antti.codes>
2020-09-22 08:56:49 +0200

    fix error on member leave

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,4 +1,5 @@
 module.exports = async (client, member) => {
+    client.db.ensure(messageDelete.guild.id, client.config.defaultSettings);
     if(client.db.has(member.guild.id, `users.${member.id}`)) {
         client.db.delete(member.guild.id, `users.${member.id}`)
     }
