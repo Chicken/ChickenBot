18f04c64c8f49446604e09055021cc2012adc504
Chicken <antti@antti.codes>
2020-09-23 19:46:08 +0200

    selfdeafened and skip fix

--- src/commands/Music/skip.js
+++ src/commands/Music/skip.js
@@ -1,12 +1,12 @@
 exports.execute = async (client, message, args) => {
-    if(!message.guild.me.voice.connection.dispatcher) return message.channel.send("I am not playing anything.");
+    if(!message.guild.me.voice?.connection?.dispatcher) return message.channel.send("I am not playing anything.");
     if(message.guild.me.voice.channel !== message.member.voice.channel) return message.channel.send("You are not in the same voice channel as me.");
     let songs = client.queues[message.guild.id];
     let num = parseInt(args[0]) || 1;
     if(isNaN(num) || num < 1 || num > songs.length) return message.channel.send("Invalid amount");
-    let removed = songs.splice(0, num-1);
+    let removed = songs.shift();
     client.queues[message.guild.id] = songs;
-    message.guild.me.voice.connection.dispatcher.destroy();;
+    message.guild.me.voice.connection.dispatcher.destroy();
     message.channel.send(`Skipping \`${removed.name}\`...`);
 };
   
---
+++ src/events/voiceStateUpdate.js
@@ -0,0 +1,5 @@
+module.exports = async (client, _, newState) => {
+    if(newState.member.id == client.user.id) {
+        newState.setSelfDeaf(true);
+    }
+}
