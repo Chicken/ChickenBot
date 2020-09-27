179894cbd151f74407bfa3c41e777ac15b0d8b63
Wilson <wilson@vmi326928.contaboserver.net>
2020-09-28 00:55:50 +0200

    Lavalink search. Play command searches properly, but is broken!

--- src/commands/Music/play.js
+++ src/commands/Music/play.js
@@ -1,6 +1,10 @@
 const ytdlDiscord = require('ytdl-core-discord');
 const ytdl = require("ytdl-core");
-const bent = require("bent")
+const bent = require("bent");
+const { MessageEmbed } = require('discord.js');
+const moment = require("moment");
+require("moment-duration-format")(moment)
+
 exports.execute = async (client, message, args) => {
     let flags = []
     while(args[0] && args[0].startsWith("-")) {
@@ -12,67 +16,84 @@ exports.execute = async (client, message, args) => {
     if(!message.member.voice.channel) return message.channel.send("You must be in a voice channel to play music!") 
     if(message.guild.me.voice.channel && message.guild.me.voice.channel !== message.member.voice.channel) return message.channel.send("I am already in a different voice channel!")
     if(!args[0]) return message.channel.send("What song should I play?")
-    let querry = args.join(" ")
-    let meta, msg;
-    
-    if(ytdl.validateURL(querry)){
-        msg = await message.channel.send("Loading video...")
-        meta = await ytdl.getBasicInfo(querry)
-    } else if(/^.*(youtu.be\/|list=)([^#\&\?]*).*/.test(querry)) {
+    let query = args.join(" ")
+    let msg = await message.channel.send('Loading...')
+    let meta;
+    const video = /(http(s)?:\/\/)?(www\.|music\.|gaming\.|m\.)?youtu(?:be\.com\/watch\?(.+=.+&)*v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?(&.+=.+)*/ // modified from https://stackoverflow.com/questions/3717115/regular-expression-for-youtube-links
+    const playlist = /(http(s)?:\/\/)?(www\.|music\.|gaming\.|m\.)?youtube\.com\/playlist\?(.+=.+&)*list=([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?(&.+=.+)*/
+ 
+    if (video.test(query) || playlist.test(query))
+        meta = await client.getVideoData(query)
+    // else if (/^.*(youtu.be\/|list=)([^#\&\?]*).*/.test(query)) {
+    //     let url = query.match(/^.*(youtu.be\/|list=)([^#\&\?]*).*/)[0]
+    //     let result = await bent(url, "string", "GET")();
+    //     let videos = result.match(/watch\?v=([a-zA-Z0-9-_]{11})/g)
+    //     if(videos.length<0){
+    //         return message.channel.send("No playlist found!")
+    //     }
+    //     videos = [...new Set(videos)]
+    //     for (let i = 0; i < videos.length; i++) {
+    //         let v = videos[i]
+    //         await (async()=>{
+    //             try{
+    //                 let vurl = "https://www.youtube.com/"+v
+    //                 if(!ytdl.validateURL(vurl)) return;
+    //                 meta = await ytdl.getBasicInfo(vurl)
+    //                 if(!meta) return;
+    //                 client.queues[message.guild.id].push({name: meta.player_response.videoDetails.title, url:vurl, length: meta.player_response.videoDetails.lengthSeconds, user: message.author.id, image: meta.player_response.videoDetails.thumbnail.thumbnails[meta.player_response.videoDetails.thumbnail.thumbnails.length-1]})
+    //             } catch(e) {return}
+    //         })()
+    //     }
+    //     let connection;
+    //     if(!message.guild.me.voice.channel) {
+    //         connection = await message.member.voice.channel.join()
+    //     } else {
+    //         return;
+    //     }
+    //     await msg.edit(`Added \`${videos.length}\` songs from \´${result.match(/<title>(.*)<\/title>/)[1].split(" - ")[0]}\` to the queue`)
+    //     playNext(connection)
+    //     return;
+    // } 
+    else
+        meta = await client.getVideoData(`ytsearch: ${query}`)
 
-        msg = await message.channel.send("Loading videos from playlist...")
-
-        let url = querry.match(/^.*(youtu.be\/|list=)([^#\&\?]*).*/)[0]
-        let result = await bent(url, "string", "GET")();
-        let videos = result.match(/watch\?v=([a-zA-Z0-9-_]{11})/g)
-        if(videos.length<0){
-            return message.channel.send("No playlist found!")
-        }
-        videos = [...new Set(videos)]
-        for (let i = 0; i < videos.length; i++) {
-            let v = videos[i]
-            await (async()=>{
-                try{
-                    let vurl = "https://www.youtube.com/"+v
-                    if(!ytdl.validateURL(vurl)) return;
-                    meta = await ytdl.getBasicInfo(vurl)
-                    if(!meta) return;
-                    client.queues[message.guild.id].push({name: meta.player_response.videoDetails.title, url:vurl, length: meta.player_response.videoDetails.lengthSeconds, user: message.author.id, image: meta.player_response.videoDetails.thumbnail.thumbnails[meta.player_response.videoDetails.thumbnail.thumbnails.length-1]})
-                } catch(e) {return}
-            })()
-        }
-        let connection;
-        if(!message.guild.me.voice.channel) {
-            connection = await message.member.voice.channel.join()
-        } else {
-            return;
-        }
-        await msg.edit(`Added \`${videos.length}\` songs from \´${result.match(/<title>(.*)<\/title>/)[1].split(" - ")[0]}\` to the queue`)
-        playNext(connection)
-        return;
-
-    } else {
-        msg = await message.channel.send("Loading video...")
-        let result = await bent(`https://www.youtube.com/results?search_query=${args.join("+")}&sp=EgIQAQ%253D%253D`, "string", "GET")();
-        let first = "https://www.youtube.com/" + result.match(/watch\?v=([a-zA-Z0-9-_]{11})/g)[0]
-        meta = await ytdl.getBasicInfo(first)
+            /*
+    {
+      "loadType": "LOAD_FAILED",
+      "playlistInfo": {},
+      "tracks": [],
+      "exception": {
+        "message": "The uploader has not made this video available in your country.",
+        "severity": "COMMON"
+      }
     }
+    */
+    let track = meta.tracks[0]?.info
 
-    let url = "https://www.youtube.com/watch?v=" + meta.player_response.videoDetails.videoId
+    let url = track.uri
 
-    if(!meta) return message.channel.send("No song found with that name!")
+    if (!meta) return message.channel.send("A unexpected error occurred!")
+    if (meta.exception) return message.channel.send(`I'm sorry an error occurred!\`\`\` ${exception.message}\`\`\``)
 
-    if(flags.includes("s")) {
-        let q = client.queues[message.guild.id];
-        let cur = q.shift();
-        q.unshift({name: meta.player_response.videoDetails.title, url:url, length: meta.player_response.videoDetails.lengthSeconds, user: message.author.id, image: meta.player_response.videoDetails.thumbnail.thumbnails[meta.player_response.videoDetails.thumbnail.thumbnails.length-1]});
-        q.unshift(cur);
-        client.queues[message.guild.id] = q;
-    } else {
-        client.queues[message.guild.id].push({name: meta.player_response.videoDetails.title, url:url, length: meta.player_response.videoDetails.lengthSeconds, user: message.author.id, image: meta.player_response.videoDetails.thumbnail.thumbnails[meta.player_response.videoDetails.thumbnail.thumbnails.length-1]})
-    }
-    
-    msg.edit(`Added \`${meta.player_response.videoDetails.title}\` to the queue.`)
+    //todo fix this
+    // if(flags.includes("s")) {c
+    //     let q = client.queues[message.guild.id];
+    //     let cur = q.shift();
+    //     q.unshift({name: meta.player_response.videoDetails.title, url:url, length: meta.player_response.videoDetails.lengthSeconds, user: message.author.id, image: meta.player_response.videoDetails.thumbnail.thumbnails[meta.player_response.videoDetails.thumbnail.thumbnails.length-1]});
+    //     q.unshift(cur);
+    //     client.queues[message.guild.id] = q;
+    // } else {
+    //     client.queues[message.guild.id].push({name: meta.player_response.videoDetails.title, url:url, length: meta.player_response.videoDetails.lengthSeconds, user: message.author.id, image: meta.player_response.videoDetails.thumbnail.thumbnails[meta.player_response.videoDetails.thumbnail.thumbnails.length-1]})
+    // }
+    const embed = new MessageEmbed()
+        .setTitle('Added to queue')
+        .setDescription(`[${track.title}](${track.uri}) by ${track.author} has been added to the queue.
+        Requested by ${message.author}
+${moment.duration(track.length).format("HH:mm:ss", { trim: false })}`)
+        .setThumbnail(`https://img.youtube.com/vi/${track.identifier}/hqdefault.jpg`)
+        .setColor('GREEN')
+
+    msg.edit('', embed)
 
 
     let connection
