af70b99793bd45d488febd275eaeec4375d09563
Chicken <antti@antti.codes>
2020-09-28 17:49:32 +0200

    added support server invite

--- src/commands/System/links.js
+++ src/commands/System/links.js
@@ -3,6 +3,7 @@ exports.execute = async (client, message, args) => {
     let embed = new Discord.MessageEmbed()
     .setTitle('Links')
     .setDescription("[Invite](https://discordapp.com/oauth2/authorize?client_id=512663078819725313&scope=bot&permissions=37088454)\n"
+                   +"[Support Server](https://discord.gg/jRN7SZB)\n"
                    +"[Website](https://chickenbot.antti.codes)\n"
                    +"[Privacy Policy](https://chickenbot.antti.codes/privacy)\n"
                    +"[Github](https://github.com/chicken/chickenbot)")
@@ -12,7 +13,7 @@ exports.execute = async (client, message, args) => {
 exports.data = {
     permissions: 18432,
     guildOnly: false,
-    aliases: ["invite", "dashboard"],
+    aliases: ["invite", "dashboard", "support"],
     name: "links",
     desc: "Useful links about the bot",
     usage: "links",
