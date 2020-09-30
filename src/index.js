c5f3b078b9ed4f6f0a8adce6e470e05b01e69775
thetayloredman <nutdriver716@gmail.com>
2020-09-30 08:42:57 -0700

    spacing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { Client } = require("discord.js");
 const client = new Client();
 
-(async()=>{
+(async () => {
     await require("./functions/clientProperties.js")(client);
     await require("./functions/util.js")(client);
     await require("./loaders/events.js")(client);
