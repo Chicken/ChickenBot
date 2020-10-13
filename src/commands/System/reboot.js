170d78dd21b97ed11fe1ba7d425a9ad21584c573
Chicken <antti@antti.codes>
2020-10-13 15:49:26 +0200

    reboot message

--- src/commands/System/reboot.js
+++ src/commands/System/reboot.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 exports.execute = async (client, message) => {
+    await message.channel.send("Stopping the bot. Rebooting if under process manager.");
     await client.handleClose();
 };
   
