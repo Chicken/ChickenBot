8e4f2668dafdf67a50a0fdfedd3026260efcf1b5
Chicken <antti@antti.codes>
2020-11-28 19:52:28 +0100

    improve the weather command

--- src/commands/Fun/weather.js
+++ src/commands/Fun/weather.js
@@ -1,27 +1,63 @@
 const bent = require("bent");
 const { MessageEmbed } = require("discord.js");
+
+function getDirectionText(dir) {
+    if(dir>=337.5||dir<22.5){
+        return "North";
+    }
+    if(dir>=22.5&&dir<67.5){
+        return "North-East";
+    }
+    if(dir>=67.5&&dir<112.5){
+        return "East";
+    }
+    if(dir>=112.5&&dir<157.5){
+        return "South-East";
+    }
+    if(dir>=157.5&&dir<202.5){
+        return "South";
+    }
+    if(dir>=202.5&&dir<247.5){
+        return "South-West";
+    }
+    if(dir>=247.5&&dir<292.5){
+        return "West";
+    }
+    if(dir>=292.5&&dir<337.5){
+        return "North-West";
+    }
+    return "ERROR";
+}
+
 // eslint-disable-next-line no-unused-vars
 exports.execute = async (client, message, args) => {
-    try {
-        if(!args[0]) {
-            message.channel.send("You need to give a location");
-        } else {
-            let apikey = client.config.weatherapi;
-            let location = encodeURI(args.join(" "));
-            let locationdecoded = decodeURI(location);
-            const getApi = bent("http://api.openweathermap.org/data/2.5/weather?q=", "GET", "json", 200);
-            let weather = await getApi(`${location}&appid=${apikey}&units=metric`);
-            const weatherEmbed = new MessageEmbed()
-                .setColor("#ff69b4")
-                .setTitle(`Weather in ${locationdecoded}`)
-                .addField(":sunny: Weather:", `${weather.weather[0].main}`)
-                .addField(":thermometer: Temperature:", `${weather.main.temp}°C`)
-                .addField(":dash: Feels like:", `${weather.main.feels_like}°C`)
-                .setTimestamp();
-            message.channel.send(weatherEmbed);
+    if(!args[0]) {
+        message.channel.send("You need to give a location");
+    } else {
+        let apikey = client.config.weatherapi;
+        let location = encodeURI(args.join(" "));
+        let weather = await bent(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}&units=metric`, "GET", "json")();
+        if(weather.cod == 404) {
+            return message.channel.send("That city doesn't exist");
         }
-    } catch (e) {
-        message.channel.send("That city doesn't exist");
+        let weatherEmbed = new MessageEmbed()
+            .setColor("#231f9c")
+            .setTitle(`Weather in ${weather.name}`)
+            .setDescription(weather.weather[0].description)
+            .addField(":sunny: Weather:", `${weather.weather[0].main}`)
+            .addField(":thermometer: Temperature:", `${weather.main.temp}°C`)
+            .addField(`${
+                weather.main.feels_like > 20 ?
+                    ":hot_face:" :
+                    weather.main.feels_like < -10 ?
+                        ":cold_face:" :
+                        ":smile:"
+            } Feels like:`, `${weather.main.feels_like}°C`)
+            .addField(":dash: Wind speed and direction:", `${weather.wind.speed}m/s, ${getDirectionText(weather.wind.deg)}`)
+            .addField("Country:", `:flag_${weather.sys.country.toLowerCase()}: ${weather.sys.country}`)
+            .setThumbnail(`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+            .setTimestamp();
+        message.channel.send(weatherEmbed);
     }
 };
 
