c91d1acd25dc5210de8efff8cb3bfcb338d4f7ba
Chicken <antti@antti.codes>
2020-11-28 16:32:09 +0100

    update user command again

--- src/commands/Miscellaneous/user.js
+++ src/commands/Miscellaneous/user.js
@@ -24,7 +24,7 @@ exports.execute = async (client, message, args) => {
         let embed = new Discord.MessageEmbed()
             .setTitle(info.tag + (info.flags && info.flags.has("SYSTEM") ? " :regional_indicator_s::regional_indicator_y::regional_indicator_s::regional_indicator_t::regional_indicator_e::regional_indicator_m:" : (info.bot ? " :regional_indicator_b::regional_indicator_o::regional_indicator_t:" : "")))
             .setThumbnail(info.displayAvatarURL({dynamic: true}))
-            .setDescription(info.toString());
+            .setDescription("Ping: " + info.toString());
         if(info.flags && info.flags.toArray().length) {
             embed.addField("Badges", info.flags.toArray().map(v => v.toLowerCase().replace(/_/g, " ")).join(", ").replace(/\b(.)/g, c => c.toUpperCase()));
         }
@@ -37,23 +37,49 @@ exports.execute = async (client, message, args) => {
 
     let roles = "‎";
     member.roles.cache.forEach(role => { if (role.name === "@everyone") return; roles += `<@&${role.id}> `; });
-    let status = info.presence.status === "dnd" ? "do not disturb" : info.presence.status;
+
+    let status = ({
+        "online": ":green_circle:",
+        "idle": ":yellow_circle:",
+        "dnd": ":red_circle:",
+        "offline": ":black_circle:"
+    })[info.presence.status];
+
+    let boosting = (member.premiumSince ? `Since ${client.formatDate(member.premiumSince)}` : "Not boosting :(");
+ 
+    let presence = "";
+    if(info.presence.activities.length != 0) {
+        presence = "**Statuses:**\n";
+        info.presence.activities.forEach(act => {
+            presence += `__*${act.name}*__\n`
+            + (act.details ? act.details + "\n" : "")
+            + (act.state ? act.state + "\n" : "")
+            + "\n";
+        });
+        presence = presence.substring(0, presence.length - 1);
+    }
+
     let embed = new Discord.MessageEmbed()
-        .setTitle(info.tag + (info.flags && info.flags.has("SYSTEM") ? " :regional_indicator_s::regional_indicator_y::regional_indicator_s::regional_indicator_t::regional_indicator_e::regional_indicator_m:" : (info.bot ? " :regional_indicator_b::regional_indicator_o::regional_indicator_t:" : "")))
+        .setTitle(info.tag + (info.flags && info.flags.has("SYSTEM") ?
+            " :regional_indicator_s::regional_indicator_y::regional_indicator_s::regional_indicator_t::regional_indicator_e::regional_indicator_m: "
+            : (info.bot ? " :regional_indicator_b::regional_indicator_o::regional_indicator_t: " : " "))
+            + status
+        )
         .setThumbnail(info.displayAvatarURL({dynamic: true}))
         .setColor(member.displayHexColor)
-        .setDescription(info.toString());
+        .setDescription("Ping: " + info.toString() + "\n\n" + presence);
     if(info.flags && info.flags.toArray().length) {
         embed.addField("Badges", info.flags.toArray().map(v => v.toLowerCase().replace(/_/g, " ")).join(", ").replace(/\b(.)/g, c => c.toUpperCase()));
     }    
-    embed.addField("Status", status, true);
-    embed.addField("Join Position", (message.guild.members.cache.array().sort((m, m2) => { return m.joinedTimestamp - m2.joinedTimestamp; }).map(m => m.id).indexOf(info.id) + 1) + "/" + message.guild.memberCount, true);
-    embed.addField("Id", info.id);
-    embed.addField("Created", client.formatDate(info.createdAt), true);
-    embed.addField("Joined", client.formatDate(member.joinedAt), true);
-    embed.addField("Roles", roles);
-    embed.setFooter(`Requested by ${message.author.tag}`);
-    embed.setTimestamp();
+    embed
+        .addField("Join Position", (message.guild.members.cache.array().sort((m, m2) => { return m.joinedTimestamp - m2.joinedTimestamp; }).map(m => m.id).indexOf(info.id) + 1) + "/" + message.guild.memberCount, true)
+        .addField("Boosting", boosting, true)
+        .addField("Id", info.id)
+        .addField("Created", client.formatDate(info.createdAt), true)
+        .addField("Joined", client.formatDate(member.joinedAt), true)
+        .addField("Roles", roles)
+        .setFooter(`Requested by ${message.author.tag}`)
+        .setTimestamp();
     message.channel.send(embed);
 };
   
