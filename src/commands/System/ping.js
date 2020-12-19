336b1f868dc1ff14a43eed58aa9a3b4c552b0415
Chicken <antti@antti.codes>
2020-12-19 11:00:17 +0100

    ping command

---
+++ src/commands/System/ping.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+exports.execute = async (client, message) => {
+    let msg = await message.channel.send("Pong!");
+    msg.edit(`🏓 Pong! The latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${client.ws.ping}ms.`);
+};
+  
+exports.data = {
+    permissions: 2048,
+    guildOnly: false,
+    aliases: ["pong"],
+    name: "ping",
+    desc: "Bot's ping to discord",
+    usage: "ping",
+    perm: 0
+};
