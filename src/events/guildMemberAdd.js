8196bfaf985e6de84f5f7eb10ad12397ea7ccc12
Chicken <antti@antti.codes>
2020-12-22 19:08:16 +0100

    member join and leave logs

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -2,14 +2,11 @@ let { MessageEmbed } = require("discord.js");
 
 module.exports = async (client, member) => {
     client.db.ensure(member.guild.id, client.config.defaultSettings);
-    if(client.db.has(member.guild.id, `users.${member.id}`)) {
-        client.db.delete(member.guild.id, `users.${member.id}`);
-    }
     let log = client.db.get(member.guild.id).settings.log;
     if(!log) return;
     let embed = new MessageEmbed()
-        .setTitle("Member left!")
-        .setColor("#c634eb")
+        .setTitle("Member joined!")
+        .setColor("#34a2eb")
         .setDescription(`${member.toString()} ${member.user.tag} (\`${member.id}\`)`)
         .setTimestamp();
     client.channels.cache.get(log).send(embed);
