af6672d9e4ac13378b908479342e02bdebe923e9
Chicken <antti@antti.codes>
2021-02-26 22:38:36 +0100

    fix move command

--- src/commands/Moderation/move.js
+++ src/commands/Moderation/move.js
@@ -7,26 +7,25 @@ exports.execute = async (client, message, args) => {
     let channel = message.mentions.channels.first();
     if (!channel) return message.channel.send("Please mention a channel.");
     await message.delete();
+
     let msgs = await message.channel.messages.fetch({ limit: amount });
-    let deletable = await msgs.filter(m => m.deletable);
-    let old = await deletable.filter(m => m.createdTimestamp < m.createdTimestamp - 1209600000);
-    await deletable.forEach(async m  => {
-        await channel.messages.fetch(m.id, true);
-    });
-    await channel.bulkDelete(deletable);
-    await old.forEach(async m => { await channel.messages.delete(m.id); });
-    await msgs.array().reverse().forEach(async msg=>{
+    let deletable = msgs.filter(m => m.deletable);
+    let old = deletable.filter(m => m.createdTimestamp < Date.now() - 1209600000);
+
+    await message.channel.bulkDelete(deletable, true);
+    for(let msg of old) msg.delete();
+
+    for(let msg of msgs.array().reverse()) {
         let embed = new Discord.MessageEmbed()
             .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
             .setDescription(msg.content)
-            .setColor(message.member.displayHexColor)
+            .setColor(msg.member.displayHexColor)
             .setTimestamp(msg.createdTimestamp);
         if (msg.attachments.size != 0) {
             embed.addField("Attachments", msg.attachments.map(v => `[${v.name}](${v.proxyURL})`).join(" "));
         }
         await channel.send(embed);
-    });
-
+    }
 };
   
 exports.data = {
