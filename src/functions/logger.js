8c3fe5cc7fe8dcee6bfb2a44a7887c116537aefc
Antti <antti@antti.codes>
2021-05-16 17:50:04 +0200

    refactor: colorette logger

--- src/functions/logger.js
+++ src/functions/logger.js
@@ -1,26 +1,24 @@
-const chalk = require("chalk");
+const { bold, magenta, greenBright, blueBright, underline, yellowBright, redBright } = require("colorette");
 
 const timeGen = () => {
     let date = new Date();
-    return chalk.magenta(`[${date.getHours().toString().padStart(2, "0")}`
-                       + `:${date.getMinutes().toString().padStart(2, "0")}`
-                       + `:${date.getSeconds().toString().padStart(2, "0")}] `);
+    return magenta(`[${date.getHours().toString().padStart(2, "0")}` + `:${date.getMinutes().toString().padStart(2, "0")}` + `:${date.getSeconds().toString().padStart(2, "0")}] `);
 };
 
 module.exports = {
     success: msg => {
-        console.log(timeGen() + chalk.greenBright(msg));
+        console.log(timeGen() + greenBright(msg));
     },
     info: msg => {
-        console.log(timeGen() + chalk.blueBright(msg));
+        console.log(timeGen() + blueBright(msg));
     },
     infoHeader: msg => {
-        console.log(timeGen() + chalk.bold.underline.blueBright(msg));
+        console.log(timeGen() + bold(underline(blueBright(msg))));
     },
     debug: msg => {
-        console.log(timeGen() + chalk.yellowBright(msg));
+        console.log(timeGen() + yellowBright(msg));
     },
     error: msg => {
-        console.log(timeGen() + chalk.redBright(msg));
+        console.log(timeGen() + redBright(msg));
     }
 };
